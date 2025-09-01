b6ff943d3c733e5fbe616f0211009f640f4b6424
Jaadari Fedi <zeko12800@gmail.com>
2025-09-01 16:58:51 +0000

    feat: added transcribe task

---
+++ app/services/transcribe/tasks.py
@@ -0,0 +1,20 @@
+import whisper
+import librosa
+from app.core.config import settings
+from app.services.celery_worker import c_worker
+from app.services.cache import CACHE
+import io
+
+model = whisper.load_model(settings.WHISPER_SIZE)
+
+@c_worker.task
+def transcribe(bytes_key: str, use_word_timestamps: bool=True) -> str:
+    """Transcribe audio and returns text segments"""
+    
+    bytes = CACHE.load(bytes_key)
+    buffer = io.BytesIO(bytes)
+
+    waveform, _ = librosa.load(buffer, mono=True, sr=int(settings.SAMPLE_RATE), dtype="float32")
+    asr_result = model.transcribe(waveform, word_timestamps=use_word_timestamps)
+    key = CACHE.save(asr_result["segments"])
+    return key
