5070c2201a506c4b746cca5353bcd415a6291823
Jaadari Fedi <zeko12800@gmail.com>
2025-09-10 10:09:06 +0000

    feat: add logging util as well as loki datasource in grafana and promtail config

---
+++ app/utils/logging.py
@@ -0,0 +1,22 @@
+import logging
+import structlog
+import sys
+
+def setup_logging():
+    # Standard logging config
+    logging.basicConfig(
+        format="%(message)s",
+        stream=sys.stdout,
+        level=logging.INFO,
+    )
+
+    structlog.configure(
+        processors=[
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.JSONRenderer(),  # 👈 JSON output
+        ],
+        wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
+        context_class=dict,
+        logger_factory=structlog.PrintLoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
